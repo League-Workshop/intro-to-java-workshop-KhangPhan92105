750940ebf6947b9f516b1a6f43b51912ee6a4d97
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-10 13:30:08 -0700

    Strings

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -1,5 +1,7 @@
 package section2;
 
+import java.util.Set;
+
 import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
@@ -13,30 +15,40 @@ public class FourSquare {
 		bob.setSpeed(600);
 
 		// 5. Set the pen width to 5
-		bob.setPenWidth(5);
+		bob.setPenWidth(5); 
 		
 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-		 for(int i=0; i<4; i++) {
+		 bob.setRandomPenColor();
+		 bob.turn(90);
+		 for(int i=0; i<4 ; i++) {
 			 
-		}
-		    // 7. Set the pen color to random
 	
-	
+		   
+		// 7. Set the pen color to random
+	          bob.setRandomPenColor();
 			// 1. Call the drawSquare() method
-		drawSquare();
+	            drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
-
-	   
-
+               bob.turn(90);
+	      } 
+	}
 	
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
 		for(int i=0; i<4; i++) {
+			bob.penDown();
 			bob.move(200);
 			bob.turn(-90);
+			bob.move(200);
+			bob.turn(-90);
+			bob.move(200);
+			bob.turn(-90);
+			bob.move(200);
+			
+			
 		}	}
 
 	public static void main(String[] args) {
