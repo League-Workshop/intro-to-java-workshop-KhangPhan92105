ced42013a7b51d965858b9a869b9805dcb6f6da3
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-17 13:26:17 -0700

    Dragon Fight

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,5 +1,7 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
@@ -7,12 +9,14 @@ public class QuizGame {
 		// 1.  Create a variable to hold the user's score 
 		
 		// 2.  Ask the user a question 
+		JOptionPane.showInputDialog(" Do we live on eart?");
 		
 		// 3.  Use an if statement to check if their answer is correct
+		if 
 		
 		// 4.  if the user's answer was correct, add one to their score 
 		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		// 5.  Create more questions by repeating steps 2, 3, and 4 below. 
 		
 		// 6.  After all the questions have been asked, print the user's score 
 		
